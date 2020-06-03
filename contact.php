d2880c82c96df897f3e2c766cd95821802280808
Satwik Arindam <59831944+Satarin19@users.noreply.github.com>
2020-06-03 13:08:12 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php
+    $name=$_POST["name"];
+    $eid=$_POST["emailid"];
+    $uname=$_POST["username"];
+    $pass=$_POST["pwd"];
+    echo"Name is : ".$name."<br>";
+    echo"Email is: ".$eid."<br>";
+    echo"Username is: ".$uname."<br>";
+    echo"Password is: ".$pass;
+?>
